7c4e383e7c821b0a4719a2e1e6f4b2e0227dcb43
Akihiko Nakamura <anakamuro@earthlink.net>
2023-10-16 18:19:11 -0400

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const inpKey = document.getElementById("inpKey");
+const inpValue = document.getElementById("inpValue");
+const btnInsert = document.getElementById("btnInsert");
+const IsOutput = document.getElementById("IsOutput");
+
+btnInsert.onclick = function(){
+    const key = inpKey.value;
+    const value = inpValue.value;
+    
+
+    if (key && value){
+        localStorage.setItem(key, value);
+        location.reload();
+    }
+}
+
+for (let i = 0; i < localStorage.length; i++){
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    IsOutput.innerHTML += `${key}: ${value}<br/>`
+}
